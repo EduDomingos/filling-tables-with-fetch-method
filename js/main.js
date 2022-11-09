dd3bb969ac3d839581c3cdca87fb82128e44a726
Eduardo Domingos <eduardo.domingos.pb@compasso.com.br>
2022-11-09 16:21:02 -0300

    Changing from XMLHttpRequest() to fetch()

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
 const requestUrl = 'https://personal-27rwjmv9.outsystemscloud.com/cardapio/rest/lanches/list'
 
 
-function getData(url) {
-  let request = new XMLHttpRequest()
-  request.open("GET", url, false)
-  request.send()
-  const json = JSON.parse(request.responseText)
+// function getData(url) {
+//   let request = new XMLHttpRequest()
+//   request.open("GET", url, false)
+//   request.send()
+//   const json = JSON.parse(request.responseText)
 
-  return json
+//   return json
+// }
+
+async function getData() {
+	const response = await fetch(requestUrl);
+	// Handle errors
+	if (!response.ok) {
+		throw new Error(`HTTP error! status: ${response.status}`);
+	}
+	const data = await response.json();
+
+  return data
 }
 
 function createRow(element) {
@@ -24,8 +35,8 @@ function createRow(element) {
 }
 
 
-function main() {
-  const data = getData(requestUrl)
+async function main() {
+  const data = await getData(requestUrl)
   const tabela = document.querySelector("#tabela")
 
   data.forEach(element => {
